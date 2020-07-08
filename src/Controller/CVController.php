3f4cfcf3d134f936a70c984897c02741142f02a8
Max Vasilishin <maxv8199@gmail.com>
2020-07-08 15:49:49 +0300

    added update method

--- src/Controller/CVController.php
+++ src/Controller/CVController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\CV;
 use App\Entity\User;
 use App\Form\CVFormType;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CVController extends AbstractController
@@ -16,22 +18,25 @@ class CVController extends AbstractController
      */
     public function index()
     {
+        $cvs = $this->getUser()->getCVs()->toArray();
+
+
         return $this->render('cv/index.html.twig', [
             'controller_name' => 'CVController',
+            'cvs'=>$cvs
         ]);
     }
     /**
-     * @Route("/resumeList/create", name="cvs")
+     * @Route("/resumeList/create", name="createcv")
      */
     public function create(Request $request){
         $CV = new CV();
-
         $form = $this->createForm(CVFormType::class,$CV);
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $CV = $form->getData();
             $this->getUser()->addCV($CV);
-            //$user->addCV($CV);
             $entity = $this->getDoctrine()->getManager();
             $entity->persist($CV);
             $entity->flush();
@@ -42,6 +47,32 @@ class CVController extends AbstractController
             ]);
         }
         return $this->render('cv/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/resumeList/edit/{id}", name="editcv")
+     * @param int $id
+     * @param CV $cv
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function edit(int $id,CV $cv,Request $request){
+        $form = $this->createForm(CVFormType::class,$cv);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $cv->setUpdatedAt(new \DateTime('now'));
+            $em = $this->getDoctrine()->getManager();
+            $cv = $form->getData();
+            $em->persist($cv);
+            $em->flush();
+            $this->addFlash('success', 'Article Created! Knowledge is power!');
+            return $this->render('default/index.html.twig',[
+                'user'=>$this->getUser()
+            ]);
+        }
+        return $this->render('cv/edit.html.twig', [
             'form' => $form->createView(),
         ]);
     }
